a2c5031822bcc60d7b7e70f4060564fe94998177
Jakub <jakub.wojtowicz@sephcode.pl>
2024-07-02 10:49:31 +0200

    Code was clean until manager requested to fuck it up

--- run-and-format.sh
+++ run-and-format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf ./artifacts/apply/job_events/*
-ansible-runner run -i apply --artifact-dir=artifacts . -p playbook_1_roles.yaml
+ansible-runner run -i apply --artifact-dir=artifacts . -p nested/playbook_1_roles.yaml
 
 cd ./artifacts/apply/job_events
 
